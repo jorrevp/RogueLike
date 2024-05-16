61a21fa7f0a4c0ead2aa6433f12e9958da9896fa
jorrevp <vanpasseljorre@gmail.com>
2024-05-16 13:26:44 +0200

    Enemies Added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    
     private static GameManager instance;
 
     private void Awake()
@@ -11,17 +12,31 @@ public class GameManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else if (instance != this)
         {
             Destroy(gameObject);
         }
+        Enemies = new List<Actor>();
     }
 
     public static GameManager Get { get => instance; }
+    public List<Actor> Enemies { get; private set; }
+    public void AddEnemy(Actor enemy)
+    {
+        Enemies.Add(enemy);
+    }
 
     public Actor GetActorAtLocation(Vector3 location)
     {
         return null;
     }
+    public GameObject CreateActor(string name, Vector2 position)
+    {
+        GameObject actor = Instantiate(Resources.Load<GameObject>($"Prefabs/{name}"), new Vector3(position.x + 0.5f, position.y + 0.5f, 0), Quaternion.identity);
+        actor.name = name;
+        return actor;
+    }
+
 }
